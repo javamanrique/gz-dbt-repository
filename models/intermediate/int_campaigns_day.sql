152a6162d1ca09b6a1a9679c6cc4733d213e4387
javier.lewagon@gmail.com <javier.lewagon@gmail.com>
2025-12-11 14:09:33 +0000

    -- int_campaigns_day.sql

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,14 +1,25 @@
  -- int_campaigns_day.sql
 
+WITH campaigns AS (
  SELECT *
- FROM {{ref('stg_gz_raw_data__adwords')}}
- UNION ALL
- SELECT *
- FROM {{ref('stg_gz_raw_data__bing')}}
- UNION ALL
- SELECT *
- FROM {{ref('stg_gz_raw_data__criteo')}}
- UNION ALL
- SELECT *
- FROM {{ref('stg_gz_raw_data__facebook')}}
- ORDER BY date_date DESC
+ FROM {{ref('int_campaigns')}} 
+),
+
+aggregated AS (
+    SELECT
+        date_date,
+        paid_source,
+        SUM(ads_cost) AS ads_cost,
+        sum(impression)    as impressions,
+        sum(click)         as clicks
+    from campaigns
+    group by
+        date_date,
+        paid_source
+)
+select
+    *
+from aggregated
+order by
+    date_date desc,
+    paid_source
