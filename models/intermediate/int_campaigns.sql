aa7dce54033c648e9058c1992798a767dbec6cdf
javier.lewagon@gmail.com <javier.lewagon@gmail.com>
2025-12-11 13:18:30 +0000

    Union all Ad campaigns

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,13 @@
+ -- int_campaigns.sql
+
+ SELECT *
+ FROM {{ref('stg_gz_raw_data__adwords')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_gz_raw_data__bing')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_gz_raw_data__criteo')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_gz_raw_data__facebook')}}
